0c977f9719a7a36ca918e4fe927835bb17e6f5fb
Aashique <mi4281864@gmail.com>
2024-08-15 14:53:24 +0500

    Add files via upload

---
+++ marks.java
@@ -0,0 +1,16 @@
+class marks{
+    public static void main(String[] args) {
+        
+    int a=1;
+    
+    
+    if (a% 2==0){
+        System.out.println("even");
+    }
+    else
+    System.out.println("odd");
+    
+    
+    }
+    
+    }
